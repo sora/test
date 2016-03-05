8b0769b6fcc24aaca10821d04499d5b82a089737
sora <sora@haeena.net>
2016-03-05 15:22:43 +0900

    add napihook

---
+++ napi/napihook.c
@@ -0,0 +1,33 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#define NAPIHOOK_VERSION  "0.0.0"
+
+#undef pr_fmt
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+
+static int __init nh_init(void)
+{
+	int rc = 0;
+
+	pr_info("napihook (%s) is loaded", NAPIHOOK_VERSION);
+
+	return rc;
+}
+module_init(nh_init);
+
+
+static void __exit nh_release(void)
+{
+	pr_info("napihook (%s) is unloaded", NAPIHOOK_VERSION);
+	return;
+}
+module_exit(nh_release);
+
+
+MODULE_AUTHOR("Yohei Kuga <sora@haeena.net>");
+MODULE_DESCRIPTION("NAPI hook");
+MODULE_LICENSE("GPL");
+MODULE_VERSION(NAPIHOOK_VERSION);
+
