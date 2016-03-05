2c86a565b7b52a2d701b0ac455241b8455fa4913
sora <sora@haeena.net>
2016-03-05 19:23:33 +0900

    netdev

--- napi/napihook.c
+++ napi/napihook.c
@@ -1,17 +1,34 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <trace/events/napi.h>
+
+struct nh_dev {
+	struct net_device *dev;
+	char dev_name[IFNAMSIZ];
+	const char *name;
+};
 
 #define NAPIHOOK_VERSION  "0.0.0"
 
-#undef pr_fmt
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+/* Module parameters, defaults. */
+//static int debug = 0;
+static char *ifname = "eth0";
 
 
 static int __init nh_init(void)
 {
+	struct nh_dev nh;
 	int rc = 0;
 
 	pr_info("napihook (v%s) is loaded", NAPIHOOK_VERSION);
+
+	nh.dev = dev_get_by_name(&init_net, ifname);
+	if (!nh.dev) {
+		pr_warn("Could not find %s\n", ifname);
+		rc = -1;
+	}
 
 	return rc;
 }
@@ -30,4 +47,7 @@ MODULE_AUTHOR("Yohei Kuga <sora@haeena.net>");
 MODULE_DESCRIPTION("NAPI hook");
 MODULE_LICENSE("GPL");
 MODULE_VERSION(NAPIHOOK_VERSION);
+
+module_param(ifname, charp, S_IRUGO);
+MODULE_PARM_DESC(ifname, "Target network device name");
 
