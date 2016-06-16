64ee8940a7c5fc21c20f064e4b764e7d6ccec7e7
sora <sora@haeena.net>
2016-06-16 15:00:52 +0900

    update

--- tlptest/rd.c
+++ tlptest/rd.c
@@ -2,8 +2,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define FNAME "/dev/pmem"
-#define ADDR  0x100000
+#define FNAME   "/dev/pmem"
+#define ADDR    0xc0004000
+#define RDSIZE  4
 
 int main (void)
 {
@@ -11,14 +12,14 @@ int main (void)
 	int fd, n;
 
 	fd = open(FNAME, O_RDONLY);
-	lseek(fd, 0x100000, SEEK_SET);
-	n = read(fd, buf, 1);
-	if (n != 1) {
+	lseek(fd, ADDR, SEEK_SET);
+	n = read(fd, buf, RDSIZE);
+	if (n != RDSIZE) {
 		printf("err: n=%d\n", n);
 		return 1;
 	}
 
-	printf("val: %02X\n", buf[0]);
+	printf("val: %02X %02X %02X %02X\n", buf[0], buf[1], buf[2], buf[3]);
 
 	close(fd);
 	return 0;
--- tlptest/wr.c
+++ tlptest/wr.c
@@ -2,25 +2,23 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define FNAME "/dev/pmem"
-#define ADDR  0x100000
-#define VAL 0xAA
+#define FNAME   "/dev/pmem"
+#define ADDR    0xc0004000
+#define VAL     0xAABBCCDD
+#define WRSIZE  4
 
 int main (void)
 {
-	unsigned char buf[0xF];
 	int fd, val = VAL;
 	size_t n;
 
 	fd = open(FNAME, O_WRONLY);
-	lseek(fd, 0x100000, SEEK_SET);
-	n = write(fd, &val, 1);
-	if (n != 1) {
+	lseek(fd, ADDR, SEEK_SET);
+	n = write(fd, &val, WRSIZE);
+	if (n != WRSIZE) {
 		printf("err: n=%d\n", (int)n);
 		return 1;
 	}
-
-	printf("val: %02X\n", buf[0]);
 
 	close(fd);
 	return 0;
